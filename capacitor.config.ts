887e44cf6e01c39fcb1982d8c4dd856b206a3862
betoarts <humbertomoura.neto@gmail.com>
2026-01-18 13:57:36 -0300

    feat: Initialize Capacitor Android project and implement a static tips service.

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.nbapark.frota',
+  appName: 'NBAPARK Frota',
+  webDir: 'dist'
+};
+
+export default config;
